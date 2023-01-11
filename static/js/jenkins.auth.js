93033e3f8c5bf466b84b566ae5271a09988a9b5f
islam kamel <islam.kamel@agr.svu.edu.eg>
2023-01-11 02:50:12 +0200

    Build User Data Model

--- static/js/jenkins.auth.js
+++ static/js/jenkins.auth.js
@@ -55,21 +55,19 @@ signform[0].password2.addEventListener("input", e => {
     e.target.setCustomValidity("error");
 })
 
-function newUser() {
-    let user = {
-        id: ++localStorage.id,
-        fname: fname.value,
-        lname: lname.value,
-        username: username.value,
-        password: password1.value,
-        orders: [],
-        productsRating: [],
-        lovedProducts: []
+function newUser(form) {
+    let userForm = {
+        fname: form.fname.value,
+        lname: form.lname.value,
+        username: form.username.value,
     };
-
-    let obj = JSON.parse(localStorage.getItem("users"))
-    obj[obj.length] = user;
-    localStorage.setItem("users", JSON.stringify(obj))
+    console.log(form)
+    let user = new User(userForm);
+    user.newPassword(form._password.value);
+    user.save();
+//    let obj = JSON.parse(localStorage.getItem("users"))
+//    obj[obj.length] = user;
+//    localStorage.setItem("users", JSON.stringify(obj))
 }
 
 function login() {
@@ -82,16 +80,57 @@ function login() {
     }
 }
 
-
+//user = new User({fname:"islam", lname:"kamel", username:"islam.kamel"})
 class User {
     constructor(data) {
-        // this.id
+        this._id = ++localStorage.id;
         this.fname = data.fname;
         this.lname = data.lname;
         this.username = data.username;
-        this.password = data.password;
-        // this.orders
-        // this.productsRating
-        // this.lovedProducts
+        this._password = "";
     }
+
+    getId() {
+        return this._id;
+    }
+
+    static hashPassword(password){
+        let hashed = [];
+        for(let code of password) {
+            code = String(code);
+            hashed[hashed.length] = code.charCodeAt();
+        }
+        return hashed.join("");
+    }
+    newPassword(password) {
+        let hashed = User.hashPassword(password);
+        console.log(hashed, password)
+        this._password = hashed;
+        return this._password;
+    };
+
+    static getUser(id) {
+        let users = JSON.parse(localStorage.getItem("users"));
+        for(let user of users) {
+            if (user._id === id) {
+                return new User(user);
+            }
+        }
+    }
+    static login(form) {
+        let data = JSON.parse(localStorage.getItem("users"));
+        for (let user of data) {
+            if (user.username === form.username && User.hashPassword(form.password) === user._password) {
+                return new User(user);
+            }
+        }
+    }
+    save() {
+        let obj = JSON.parse(localStorage.getItem("users"))
+        obj[obj.length] = this;
+        localStorage.setItem("users", JSON.stringify(obj))
+    }
+
 }
+
+//user = new User({fname:"islam", lname:"kamel", username:"islam.kamel"})
