50be662708382368e5486f0762e1239748dc0c0c
islam kamel <islam.kamel@agr.svu.edu.eg>
2023-01-13 02:37:01 +0200

    Build User, loveproduct and productRatings class

--- static/js/jenkins.auth.js
+++ static/js/jenkins.auth.js
@@ -1,5 +1,6 @@
-const signform = document.querySelectorAll('.needs-validation[name="signupForm"]');
-const loginform = document.querySelectorAll('.needs-validation[name="loginForm"]');
+const signform = document.forms.signupForm;
+const loginform = document.forms.loginForm;
+
 const data = JSON.parse(localStorage.getItem("users"));
 
 if (!localStorage.getItem("users")) {
@@ -9,80 +10,123 @@ if (!localStorage.getItem("id")) {
     localStorage.setItem("id", 0);
 }
 
-(() => {
-    'use strict'
-    Array.from(loginform).forEach(form => {
-        form.addEventListener('submit', event => {
-            event.preventDefault()
-            if (!form.checkValidity()) {
-                event.stopPropagation()
-            }
-            console.log({username:form.usernameLogin.value, password: form.passwordLogin.value})
-            let user = User.login({username:form.usernameLogin.value, password: form.passwordLogin.value});
-            console.log(user)
-            if (user) {
-                userInfo.innerHTML = user.username;
-            }
-            form.classList.add('was-validated')
-        }, false)
-    })
-})();
+function loginError() {
+    let error = document.querySelector("#loginForm > .invalid-feedback");
+    error.classList.add("d-block");
+}
 
-(() => {
-    'use strict'
-    Array.from(signform).forEach(form => {
+loginform.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let login = User.login({username: loginform.usernameLogin.value, password: loginform.passwordLogin.value});
 
-        form.addEventListener('submit', event => {
-            event.preventDefault()
-            if (!form.checkValidity()) {
-                event.stopPropagation()
-            }
-            if (data.length > 0) {
-                for (let user of data) {
-                    if (user.username !== form.username.value) {
-                       newUser(form);
-                    }
-                }
-            } else {
-                newUser(form);
-                form.classList.add('was-validated')
-            }
+    if (!loginform.checkValidity()) {
+        e.stopPropagation();
+    }
+    loginform.classList.add('was-validated');
+    if (login) {
+        userInfo.innerHTML = "@"+loginform.usernameLogin.value;
+        document.querySelector("#loginForm > div.col-12 > div > button.btn.btn-secondary").click();
+        saveLocalStorage("login", `{username: ${login.username}}`)
+    } else {
+        loginError();
+    }
 
-        }, false)
- })
-})();
 
-signform[0].password2.addEventListener("input", e => {
-    if (e.target.value === signform[0].password1.value){
+})
+
+signform.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (!signform.checkValidity()) {
+        e.stopPropagation();
+    } else {
+        newUser(signform);
+    }
+    signform.classList.add('was-validated');
+
+})
+
+signform.password2.addEventListener("input", e => {
+    if (e.target.value === signform.password1.value){
         return e.target.setCustomValidity("");
     }
     e.target.setCustomValidity("error");
 })
 
+
 function newUser(form) {
-    let userForm = {
-        fname: form.fname.value,
-        lname: form.lname.value,
-        username: form.username.value,
-    };
-    let user = new User(userForm);
-    user.newPassword(form._password.value);
-    user.save();
+    let formInput  = form.querySelectorAll("input");
+    let user = {};
+    for (let key of formInput) {
+        user[key.id] = key.value;
+    }
+    let create = new User(user);
+    create.newPassword(form._password.value);
+    return create.save();
 }
 
+class ProductRatings {
+    #ratings;
+    constructor() {
+        this.#ratings = {};
+    }
 
-//user = new User({fname:"islam", lname:"kamel", username:"islam.kamel"})
+    setRating(username, productId, rating) {
+        let products = this.#ratings[username];
+        if (products === undefined) {
+            return this.#ratings[username] = JSON.parse(`{
+                "${productId}": ${rating}
+            }`);
+        }
+        return this.#ratings[username][productId] = rating;
+    }
+
+    getAll() {
+        return this.#ratings;
+    }
+
+    remove(username, prodeuctId) {
+        let ratings = this.getAll(username);
+        delete ratings[username][prodeuctId];
+        this.#ratings[username] = ratings[username];
+    }
+}
+class LovedPorducts {
+    #products;
+    constructor() {
+        this.#products = {};
+    }
+
+    love(username, productId) {
+        let products = this.#products[username];
+        if (products === undefined) {
+            return this.#products[username] = [productId];
+        }
+        products[products.length] = productId
+        return this.#products[username] = products;
+    }
+
+    unlove(username, productId) {
+        let products = this.#products[username];
+        let index = products.indexOf(productId);
+        if (index > -1) {
+            products.pop(products.indexOf(productId));
+            return this.#products[username] = products;
+        }
+    }
+
+    getAll() {
+        return this.#products;
+    }
+}
+
 class User {
     constructor(data) {
-        this._id = ++localStorage.id;
         this.fname = data.fname;
         this.lname = data.lname;
         this.username = data.username;
+        this.lovedProducts = new LovedPorducts();
+        this.productRatings = new ProductRatings();
         this._password = "";
-    }
-
-    getId() {
-        return this._id;
     }
 
     static hashPassword(password){
@@ -100,30 +144,43 @@ class User {
         return this._password;
     };
 
-    static getUser(id) {
+    static getUser(username) {
         let users = JSON.parse(localStorage.getItem("users"));
         for(let user of users) {
-            if (user._id === id) {
+            if (user.username === username) {
                 return new User(user);
             }
         }
     }
-    static login(form) {
+    static login(obj) {
         let data = JSON.parse(localStorage.getItem("users"));
         for (let user of data) {
-            if (user.username === form.username && User.hashPassword(form.password) === user._password) {
+            if (user.username === obj.username && User.hashPassword(obj.password) === user._password) {
                 return new User(user);
             }
         }
         return false;
     }
+
+    #vaildUser() {
+        let users = getItemFromLocalStorage("users");
+        for (let user of users) {
+            if (user.username === this.username) {
+                return false
+            }
+        }
+        return true;
+    }
+
     save() {
         let obj = JSON.parse(localStorage.getItem("users"));
-        obj[obj.length] = this;
-        saveLocalStorage("users", JSON.stringify(obj));
-//        localStorage.setItem("users", JSON.stringify(obj));
+        if (this.#vaildUser()) {
+            obj[obj.length] = this;
+            saveLocalStorage("users", JSON.stringify(obj));
+            return this;
+        }
+        return false;
     }
 
 }
-
 //user = new User({fname:"islam", lname:"kamel", username:"islam.kamel"})
--- static/js/jenkins.storage.js
+++ static/js/jenkins.storage.js
@@ -1,38 +1,16 @@
-
-function createCookie(cookieValue) {
-    let date = new Date();
-    const expire = date.getTime() + 1000 * 60 * 60 * 24;
-    date.setTime(expire);
-    document.cookie = `name=${cookieValue}; expires=${date};`;
-}
-
-function readCookie(key) {
-    let cookie = document.cookie.split("; ");
-    for (let c of cookie) {
-        if (c.split("=")[0] === key) {
-            return c.split("=")[1];
-        }
-    }
-    return 0;
-}
-
-function removeCookie(key) {
-    let cookie = readCookie(key);
-
-    if (cookie) {
-        let date = new Date(0);
-        document.cookie = `${key}=; expires=${date.toUTCString()}`;
-    } else {
-        return 0;
-    }
-}
-
 function saveLocalStorage(key, value) {
     localStorage.setItem(key, value);
 }
 
 function getItemFromLocalStorage(key) {
-    return localStorage.getItem(key);
+    return JSON.parse(localStorage.getItem(key));
+}
+
+function checkItem(key) {
+    let res = getItemFromLocalStorage(key);
+    if (res) {
+        return true;
+    }
 }
 
 function removeItemFromLocalStorage(key) {
